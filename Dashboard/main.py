50c1eac2d9307eb0c968c7e5b5fd34b17e626494
William Martindale <whyjamuh@outlook.com>
2023-12-18 19:13:24 -0700

    clarify some language

--- Dashboard/main.py
+++ Dashboard/main.py
@@ -13,15 +13,17 @@ df.rename(columns = {'energy_cons': 'Energy Consumption',
                     'service_per': 'Percent Services'},inplace = True)
 
 st.title("Li Keqiang Index Project---Data Exploration")
+st.markdown('This dashboard is designed to explore the dataset used for my Li Keqiang Index project. For more on the project, [click here](https://wijama.org/2023/12/15/Li-Keqiang-Index-Introduction-and-EDA.html).')
+
 
 st.header('Explore Quantitative Variables')
 
 st.subheader('Univariate Exploration')
-st.markdown('Examine the univariate distribution of each quantitative variable. [See examples from EDA.](https://wijama.org/2023/12/15/Li-Keqiang-Index-Introduction-and-EDA.html#:~:text=Exploratory%20Data%20Analysis)')
+st.markdown('Examine the univariate distribution of each quantitative variable. [See examples from EDA](https://wijama.org/2023/12/15/Li-Keqiang-Index-Introduction-and-EDA.html#:~:text=Exploratory%20Data%20Analysis).')
 selected_var = st.selectbox('Select a Variable', ['Year','Democracy Index','Energy Consumption','Railway Cargo','Percent Urban','Percent Services','GDP'])
 hist_series = df[selected_var]
 
-if st.checkbox('Log variable'):
+if st.checkbox('Log {selected_var}'):
     fig = px.histogram(x= [math.log(item) for item in hist_series])
     st.plotly_chart(fig)
 else:
@@ -29,7 +31,7 @@ else:
     st.plotly_chart(fig)
 
 st.subheader("Bivariate Exploration")
-st.markdown('Examine quantitative variables in relation to each other. [For examples, see pair plot from EDA](https://wijama.org/2023/12/15/Li-Keqiang-Index-Introduction-and-EDA.html#:~:text=pair%20plot)')
+st.markdown('Examine quantitative variables in relation to each other. [For examples, see pair plot from EDA](https://wijama.org/2023/12/15/Li-Keqiang-Index-Introduction-and-EDA.html#:~:text=pair%20plot).')
 scatter_var1, scatter_var2 = st.multiselect('Select Two Variables', ['Year','Democracy Index','Energy Consumption','Railway Cargo','Percent Urban','Percent Services','GDP'], ['Railway Cargo', 'GDP'], max_selections = 2)
 if st.checkbox(f'Log {scatter_var1}'):
     scatter_series1 = [math.log(item) for item in df[scatter_var1]]
@@ -40,11 +42,11 @@ if st.checkbox(f'Log {scatter_var2}'):
 else: 
     scatter_series2 = df[scatter_var2]
 
-if st.checkbox('Trendline'):
+if st.checkbox('Insert Trendline'):
     scatter_trendline="ols"
 else:
     scatter_trendline=None
-if st.checkbox('Color Region'):
+if st.checkbox('Color points by Region'):
     color_region = "Region"
 else:
     color_region = None
@@ -56,7 +58,7 @@ scatter_fig = px.scatter(df, x=scatter_series1, y=scatter_series2, color = color
 st.plotly_chart(scatter_fig)
 
 st.subheader('Count of Data Points by Region')
-st.markdown('Examine the number of data points from each region. [See comments on this in EDA.](https://wijama.org/2023/12/15/Li-Keqiang-Index-Introduction-and-EDA.html#:~:text=A%20countplot%20of%20data)')
+st.markdown('Examine the number of data points from each region. [See a discussion on this in EDA.](https://wijama.org/2023/12/15/Li-Keqiang-Index-Introduction-and-EDA.html#:~:text=A%20countplot%20of%20data)')
 reg_count = px.histogram(df, y= 'Region', barmode='group')
 st.plotly_chart(reg_count)
 
